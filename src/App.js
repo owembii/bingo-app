1e52eb283c4c6e3920a9c59feefb8d628469316f
ced <cedrickhermoso12345@gmail.com>
2023-10-06 06:53:55 +0800

    bingo react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 function App() {
+  const [gameCode, setGameCode] = useState('');
+
+  const handleGameCodeChange = (e) => {
+    setGameCode(e.target.value);
+  };
+
+  const getBingoCardURL = () => {
+    // Construct the URL based on the game code entered
+    return `/Bingo-Card`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Router>
+      <div className="App">
+        <h1>BINGO Game</h1>
+        <p>Enter the BINGO game code to get your Bingo card:</p>
+        <input
+          label="Game Code"
+          variant="outlined"
+          value={gameCode}
+          onChange={handleGameCodeChange}
+        />
+        <br />
+        <a href={getBingoCardURL()} target="_blank" rel="noopener noreferrer">
+          <button variant="contained" color="primary">
+            Get Bingo Card
+          </button>
         </a>
-      </header>
-    </div>
+      </div>
+    </Router>
   );
 }
 
